d90027c53c7f4f59fc06a04e6fdf2c1eef5bd575
DavisDoro <deivs65@gmail.com>
2021-10-20 14:59:55 +0300

    Add project files.

---
+++ BlazorTicTac/Client/Services/FieldService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTicTac.Client.Services
+{
+    public class FieldService : IFieldService
+    {
+        public IList<int> OccupiedFields { get; set; } = new List<int>();
+
+        public void ConfirmFields(int fieldId)
+        {
+            OccupiedFields.Add(fieldId);
+
+        }
+
+        public bool CheckField(int fieldId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetField()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorTicTac/Client/Services/IFieldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BlazorTicTac.Shared;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTicTac.Client.Services
+{
+    public interface IFieldService
+    {
+        IList<int> OccupiedFields { get; set; }
+        void ConfirmFields(int fieldId);
+        void ResetField();
+        bool CheckField(int fieldId);
+    }
+}
