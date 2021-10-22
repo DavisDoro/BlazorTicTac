9329d365de817df0a54005c192706c979911fdd7
Davis Doro(home) <davisdoro@outlook.com>
2021-10-22 18:01:13 +0300

    Basic game done

--- BlazorTicTac/Client/Program.cs
+++ BlazorTicTac/Client/Program.cs
@@ -19,7 +19,7 @@ namespace BlazorTicTac.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<IFieldService, FieldService>();
+            builder.Services.AddScoped<TTLogic>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorTicTac/Client/Services/TTLogic.cs
@@ -0,0 +1,147 @@
+﻿namespace BlazorTicTac.Client.Services
+{
+    public class TTLogic
+    {
+        public int playerId = 1;
+        public bool winner = false;
+        public bool invalidMove = false;
+        public int[,] gameField = new int[3, 3] {
+            { 0, 0, 0 },
+            { 0, 0, 0, },
+            { 0, 0, 0, }
+        };
+
+        public bool lastMove = false;
+        int lastRow;
+        int lastCol;
+
+        public void Mark(int row, int col, int playerId)  // validate and place your mark on board
+        {
+            if (winner)
+            {
+                return;
+            }
+            if (gameField[row, col] == 1 || gameField[row, col] == 2)
+            {
+                return;
+            }
+            if (lastMove) // reset previous field on board if player chose multiple
+            {
+                gameField[lastRow, lastCol] = 0;
+            }
+
+            gameField[row, col] = playerId;
+
+            lastMove = true;    
+            invalidMove = false;
+
+            lastRow = row;   //obtain last selected cell
+            lastCol = col;
+        }
+
+        public void ConfirmMove() // confirm button
+        {
+            if (!lastMove)
+            {
+                invalidMove = true;
+                return;
+            }
+            lastMove = false;
+            if (HasWon(playerId))
+            {
+                winner = true;
+                return;
+            }
+            if (playerId == 1)
+            {
+                playerId = 2;
+            }
+            else
+            {
+                playerId = 1;
+            }
+
+        }
+
+        public void Reset() // reset game board
+        {
+            gameField = new int[3, 3] {
+            { 0, 0, 0 },
+            { 0, 0, 0, },
+            { 0, 0, 0, }
+            };  
+            winner = false;
+        }
+
+        public bool HasWon(int playerId) // check for winner !! MIGHT be worth splitting in 3 seperate methods
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++) // horizontal check
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameField[i, j] == playerId)
+                    {
+                        count++;
+                    }
+                    if (count == 3)
+                    {
+                        count = 0;
+                        return true;
+                    }
+                }
+                count = 0;
+            }
+            for (int i = 0; i < 3; i++) // vertical check
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameField[j, i] == playerId)
+                    {
+                        count++;
+                    }
+                    if (count == 3)
+                    {
+                        count = 0;
+                        return true;
+                    }
+                }
+                count = 0;
+            }
+            // [0 0] [1 1] [2 2]
+            // [0 2] [1 1] [2 0]
+            for (int i = 0; i < 3; i++) //diag one
+            {
+                if (gameField[i, i] == playerId)
+                {
+                    count++;
+                }
+                if (count == 3)
+                {
+                    count = 0;
+                    return true;
+                }
+            }
+            int h = 2;
+            count = 0;
+            for (int i = 0; i < 3; i++) // diag two
+            {
+                
+                    if (gameField[i, h] == playerId)
+                    {
+                        count++;
+                    }
+
+                    if (count == 3)
+                    {
+                    count = 0;
+                    return true;
+                    }
+                h--;
+            }
+            return false;
+        }
+    }
+}
